2c519cb072692a35245d34311d7895a4e9f4749f
thilo <thiloschmid@bluewin.ch>
2020-06-22 17:25:34 +0200

    made it read the ipv6 address automatically

--- start_server_build.py
+++ start_server_build.py
@@ -1,11 +1,14 @@
 import uvicorn
 from app.main import sio_app
 
+from requests import get
+
+ipv6_address = get('https://ip6.seeip.org/').text
 
 if __name__ == "__main__":
     uvicorn.run(
         'app.main:sio_app', 
-        host="2a04:ee41:3:b127:f7a3:1ea5:2c09:ef42", 
+        host=ipv6_address, 
         port=8049, 
         # reload=True,
         log_level='info')
